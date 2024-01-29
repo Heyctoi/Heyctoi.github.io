f7d9333b88e965aac91294a57b791c17206e2e52
Heyctoi <ayoubkhloufi1@gmail.com>
2024-01-30 00:02:37 +0100

    a

--- script/main.js
+++ script/main.js
@@ -1,11 +1,5 @@
 const ics_events = [];
 
 async function fetch_events(){
-
-<<<<<<< HEAD
-    return  fetch('http://heyctoi.unaux.com/Projets/Calendar/events.json')
-=======
-    return  fetch('https://heyctoi.github.io/events.json')
->>>>>>> cdea53251311c9dd717cffe464ff75d1e38d3a2d
-        .then((response) => response.json())
+    return  fetch('https://heyctoi.github.io/events.json').then((response) => response.json())
 }
